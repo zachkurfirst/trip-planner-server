5e133d781847e95f29f323259ae88b1c92157507
Zach Kurfirst <zachmk11@gmail.com>
2023-12-19 16:45:44 -0500

    implement search function to fetch locations

--- models/trip.js
+++ models/trip.js
@@ -7,7 +7,7 @@ const TripSchema = new Schema(
     name: { type: String, required: true, maxlengh: 25 },
     description: { type: String, required: true, maxlength: 100 },
     // TODO: location object with fields for city, state?
-    location: { type: String, required: true },
+    location: { type: String },
     // TODO: validation for date fields - no past dates, endDate must come after startDate
     startDate: { type: Date },
     endDate: { type: Date },
