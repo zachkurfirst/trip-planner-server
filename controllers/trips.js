3a014fc3f2d3a2b4c04fb59b2a505f9b1dc141a9
bsong1124 <bsong1124@gmail.com>
2023-12-20 14:54:47 -0500

    image fetch per result in search fetch

--- controllers/trips.js
+++ controllers/trips.js
@@ -41,16 +41,32 @@ async function destroy(req, res, next) {
 
 async function search(req, res) {
   const q = req.query.q;
-  console.log("search q: ", q);
+  // console.log("search q: ", q);
   try {
     let endpoint = `${ROOT_URL}/search?key=${token}&searchQuery=${q}&language=en`;
-    console.log("endpoint: ", endpoint);
+    // console.log("endpoint: ", endpoint);
     const findSearchResponse = await fetch(endpoint, {
       method: "GET",
     });
     const data = await findSearchResponse.json();
-    console.log("DATA", data.data);
-    res.json(data.data);
+    const allData = data.data;
+    // console.log({ allData });
+    // console.log(allData[0].location_id);
+    // res.json(allData);
+
+    const imagePromises = allData.map(async (e) => {
+      let imageEndpoint = `${ROOT_URL}/${e.location_id}/photos?key=${token}&language=en`;
+      // console.log({imageEndpoint});
+      // console.log("LOCATION_ID", e.location_id);
+      const images = await fetch(imageEndpoint, { method: "GET" });
+      const imagesData = await images.json();
+      const allImagesData = imagesData.data;
+      console.log({ allImagesData });
+      return allImagesData[0].images.large;
+    });
+
+    const imageData = await Promise.all(imagePromises);
+    res.json({ allData, imageData });
   } catch (err) {
     console.log(err);
     res.json({ message: "error", error: res.statusText });
