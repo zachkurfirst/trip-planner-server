ac386b8ec56fad7d000eb1738951e46f95c62a30
bsong1124 <bsong1124@gmail.com>
2023-12-21 12:37:46 -0500

    update activities search function

--- controllers/activities.js
+++ controllers/activities.js
@@ -7,22 +7,22 @@ const search = async (req, res) => {
   const q = req.query.q;
   // console.log("search q: ", q);
   try {
-    let endpoint = `${ROOT_URL}/search?key=${token}&searchQuery=${q}&language=en`;
+    // let endpoint = `${ROOT_URL}/search?key=${token}&searchQuery=${q}&language=en`;
     let detailEndpoint = `${ROOT_URL}/${q}/details?key=${token}&language=en&currency=USD`;
-    // let nearbyEndpoint = `${ROOT_URL}/nearby_search?latLong=${latitude}%2C%20${longitude}&key=${token}&category=attractions&radius=15&radiusUnit=mi&language=en`;
+    let nearbyEndpoint = `${ROOT_URL}/nearby_search?latLong=${detailData.latitude}%2C%20${detailData.longitude}&key=${token}&category=attractions&radius=15&radiusUnit=mi&language=en`;
     // console.log("endpoint: ", endpoint);
     const detailResponse = await fetch(detailEndpoint, {
       method: "GET",
     });
-
     const detailData = await detailResponse.json();
     // console.log("LAT", detailData.latitude);
     // console.log("LONG", detailData.longitude);
-
-    res.json({
-      latitude: detailData.latitude,
-      longitude: detailData.longitude,
+    const nearbyResponse = await fetch(nearbyEndpoint, {
+      method: "GET",
     });
+    const nearbyData = await nearbyResponse.json();
+
+    res.json(nearbyData);
   } catch (err) {
     console.log(err);
     res.json({ message: "error", error: res.statusText });
