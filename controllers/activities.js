6b3945a5aeebe10b70269db41b00b1825dd6ed9f
bsong1124 <bsong1124@gmail.com>
2023-12-21 09:36:31 -0500

    change activities search function to return only lat/long

--- controllers/activities.js
+++ controllers/activities.js
@@ -3,22 +3,26 @@ const { Trip } = require("../models");
 const token = process.env.TRIPADVISOR_TOKEN;
 const ROOT_URL = "https://api.content.tripadvisor.com/api/v1/location";
 
-const search = async () => {
+const search = async (req, res) => {
   const q = req.query.q;
   // console.log("search q: ", q);
   try {
     let endpoint = `${ROOT_URL}/search?key=${token}&searchQuery=${q}&language=en`;
-    let detailEndpoint = `${ROOT_URL}/${location_id}/details?key=${token}&language=en&currency=USD`;
-    let nearbyEndpoint = `${ROOT_URL}/nearby_search?latLong=${latitude}%2C%20${longitude}&key=${token}&category=attractions&radius=15&radiusUnit=mi&language=en`;
+    let detailEndpoint = `${ROOT_URL}/60763/details?key=${token}&language=en&currency=USD`;
+    // let nearbyEndpoint = `${ROOT_URL}/nearby_search?latLong=${latitude}%2C%20${longitude}&key=${token}&category=attractions&radius=15&radiusUnit=mi&language=en`;
     // console.log("endpoint: ", endpoint);
     const detailResponse = await fetch(detailEndpoint, {
       method: "GET",
     });
 
     const detailData = await detailResponse.json();
-    console.log(detailData)
+    // console.log("LAT", detailData.latitude);
+    // console.log("LONG", detailData.longitude);
 
-    res.json(detailData);
+    res.json({
+      latitude: detailData.latitude,
+      longitude: detailData.longitude,
+    });
   } catch (err) {
     console.log(err);
     res.json({ message: "error", error: res.statusText });
