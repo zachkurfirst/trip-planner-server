7351a27180d62425c3494e4949e1aa0dc7a99471
bsong1124 <bsong1124@gmail.com>
2023-12-21 14:18:40 -0500

    fix endpoint declaration before values being read

--- controllers/activities.js
+++ controllers/activities.js
@@ -4,23 +4,27 @@ const token = process.env.TRIPADVISOR_TOKEN;
 const ROOT_URL = "https://api.content.tripadvisor.com/api/v1/location";
 
 const search = async (req, res) => {
+  console.log("WORKIGNWSGASDFGHH");
   const q = req.query.q;
-  // console.log("search q: ", q);
+  console.log("search q: ", q);
   try {
     // let endpoint = `${ROOT_URL}/search?key=${token}&searchQuery=${q}&language=en`;
     let detailEndpoint = `${ROOT_URL}/${q}/details?key=${token}&language=en&currency=USD`;
-    let nearbyEndpoint = `${ROOT_URL}/nearby_search?latLong=${detailData.latitude}%2C%20${detailData.longitude}&key=${token}&category=attractions&radius=15&radiusUnit=mi&language=en`;
     // console.log("endpoint: ", endpoint);
     const detailResponse = await fetch(detailEndpoint, {
       method: "GET",
     });
     const detailData = await detailResponse.json();
-    // console.log("LAT", detailData.latitude);
-    // console.log("LONG", detailData.longitude);
+    let nearbyEndpoint = `${ROOT_URL}/nearby_search?latLong=${detailData.latitude}%2C%20${detailData.longitude}&key=${token}&category=attractions&radius=15&radiusUnit=mi&language=en`;
+    console.log("LAT", detailData.latitude);
+    console.log("LONG", detailData.longitude);
     const nearbyResponse = await fetch(nearbyEndpoint, {
       method: "GET",
     });
     const nearbyData = await nearbyResponse.json();
+    console.log({ nearbyData });
+    console.log({ detailEndpoint });
+    console.log({ nearbyEndpoint });
 
     res.json(nearbyData);
   } catch (err) {
