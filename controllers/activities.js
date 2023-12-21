fe14adbeded80025865e325c1c1e1a9c870825e0
bsong1124 <bsong1124@gmail.com>
2023-12-20 20:49:35 -0500

    add details and nearbySearch endpoints

--- controllers/activities.js
+++ controllers/activities.js
@@ -4,35 +4,27 @@ const token = process.env.TRIPADVISOR_TOKEN;
 const ROOT_URL = "https://api.content.tripadvisor.com/api/v1/location";
 
 const search = async () => {
-    const q = req.query.q;
-    // console.log("search q: ", q);
-    try {
-      let endpoint = `${ROOT_URL}/search?key=${token}&searchQuery=${q}&language=en`;
-      // console.log("endpoint: ", endpoint);
-      const findSearchResponse = await fetch(endpoint, {
-        method: "GET",
-      });
-      const data = await findSearchResponse.json();
-      const allData = data.data;
-      // console.log({ allData });
-      // console.log(allData[0].location_id);
-      // res.json(allData);
-  
-      const imagePromises = allData.map(async (e) => {
-        let imageEndpoint = `${ROOT_URL}/${e.location_id}/photos?key=${token}&language=en`;
-        // console.log({imageEndpoint});
-        // console.log("LOCATION_ID", e.location_id);
-        const images = await fetch(imageEndpoint, { method: "GET" });
-        const imagesData = await images.json();
-        const allImagesData = imagesData.data;
-        console.log({ allImagesData });
-        return allImagesData[0].images.large;
-      });
-  
-      const imageData = await Promise.all(imagePromises);
-      res.json({ allData, imageData });
-    } catch (err) {
-      console.log(err);
-      res.json({ message: "error", error: res.statusText });
-    }
+  const q = req.query.q;
+  // console.log("search q: ", q);
+  try {
+    let endpoint = `${ROOT_URL}/search?key=${token}&searchQuery=${q}&language=en`;
+    let detailEndpoint = `${ROOT_URL}/${location_id}/details?key=${token}&language=en&currency=USD`;
+    let nearbyEndpoint = `${ROOT_URL}/nearby_search?latLong=40.713238%2C%20-74.00584&key=${token}&category=attractions&radius=15&radiusUnit=mi&language=en`;
+    // console.log("endpoint: ", endpoint);
+    const detailResponse = await fetch(detailEndpoint, {
+      method: "GET",
+    });
+
+    const data = await detailResponse.json();
+    const imageData = await Promise.all(imagePromises);
+
+    res.json({ data, imageData });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: "error", error: res.statusText });
   }
+};
+
+module.exports = {
+  search,
+};
